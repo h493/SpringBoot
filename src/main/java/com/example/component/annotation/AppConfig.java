98e6c837c3ef9c8428e58fb908589273c123cf95
h493 <chhikarahimanshu21@gmail.com>
2024-10-27 14:12:03 +0530

    Spring FrameWork || Working with Spring Annotations

---
+++ src/main/java/com/example/component/annotation/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.component.annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.component.annotation")
+public class AppConfig {
+}
